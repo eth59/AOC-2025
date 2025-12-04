9986cdca0ea3d36ad685f58cb0be7bd6046bf96c
eth59 <ethanq.portable@free.fr>
2025-12-04 17:56:03 +0100

    day 1 part 2

--- day1/part2.go
+++ day1/part2.go
@@ -9,6 +9,7 @@ import (
 func partTwo(input string) int {
 	current_position := 50
 	res := 0
+	already_counted := false
 
 	for _, line := range strings.Split(input, "\n") {
 
@@ -22,22 +23,31 @@ func partTwo(input string) int {
 			panic(err)
 		}
 
-		for i := 0; i < nb; i++ {
-			if line[0] == 'L' {
-				current_position -= 1
-				if current_position == 0 {
+		res += nb / 100
+		nb -= (nb / 100) * 100
+
+		if current_position == 0 {
+			already_counted = true
+		}
+
+		if line[0] == 'L' {
+			current_position -= nb
+			if current_position <= 0 {
+				if !already_counted {
 					res++
-				} else if current_position < 0 {
+				}
+				if current_position < 0 {
 					current_position += 100
 				}
-			} else {
-				current_position += 1
-				if current_position == 100 {
-					res++
-					current_position = 0
-				}
 			}
- 		}
+		} else {
+			current_position += nb
+			if current_position >= 100 {
+				res++
+				current_position -= 100
+			}
+		}
+		already_counted = false
 	}
 
 	return res
